81d71bc73e06d2a69e4b3b828ebe9c491d28594d
Simon Rojas <srojasv10@gmail.com>
2018-10-05 10:02:34 -0400

    Add post request

--- resources.py
+++ resources.py
@@ -6,15 +6,21 @@ class UpvoteResource():
     def on_get(self, req, resp):
         item_name = req.params['item_name']
 
-        item_votes.setdefault(item_name, 0)
-        item_votes[item_name] += 1
+        self.upvote(item_name)
 
         resp.body = "{} now has {} votes".format(item_name, item_votes[item_name])
 
     def on_post(self, req, resp):
         json_data = req.media
+        item_name = json_data['item_name']
 
-        resp.body = "I received this: {}".format(json_data)
+        self.upvote(item_name)
+
+        resp.body = "{} now has {} votes".format(item_name, item_votes[item_name])
+
+    def upvote(self, item_name):
+        item_votes.setdefault(item_name, 0)
+        item_votes[item_name] += 1
 
 
 class DownvoteResource():
